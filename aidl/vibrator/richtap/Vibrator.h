2ea55bc6382c9e75d1918be6cd415f971f6e3f08
actuallyaryaman <72538446+actuallyaryaman@users.noreply.github.com>
2026-07-15 06:14:30 +0000

    Revert "richtap: Add support for Oplus Awinic sysfs nodes"

--- aidl/vibrator/richtap/Vibrator.h
+++ aidl/vibrator/richtap/Vibrator.h
@@ -44,11 +44,6 @@ class Vibrator : public BnVibrator {
     ndk::ScopedAStatus getSupportedBraking(std::vector<Braking>* supported) override;
     ndk::ScopedAStatus composePwle(const std::vector<PrimitivePwle>& composite,
                                    const std::shared_ptr<IVibratorCallback>& callback) override;
-
-  private:
-#ifdef USES_OPLUS_AWINIC
-    bool mUseSysfsOnOff = false;
-#endif
 };
 
 }  // namespace vibrator
